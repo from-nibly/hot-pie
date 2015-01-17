0c1367682c905ce566fdd55a61b9d1c3e33a6bae
Landon1013 <landon@thecallfamily.com>
2015-01-17 00:14:44 -0700

    persists override

--- angular/app/scripts/services/thermostatservice.js
+++ angular/app/scripts/services/thermostatservice.js
@@ -8,23 +8,22 @@
  * Service in the hotPieApp.
  */
 angular.module('hotPieApp')
-  .service('thermostatService', function ($http) {
+    .service('thermostatService', function ($http) {
 
-    this.getCurrentTemp = function() {
-        return $http({
-            method : "GET",
-            url : "http://137.190.207.214:3000/temp/current" + name
-        });
-    };
-//
-//        this.setCurrentTemp = function() {
-//
-//            var override = "http://137.190.207.214:3000/temp/override/";
-//            console.log(override);
-//            var targetTemp = override.newValue;
-//            return $http({
-//                method:"POST",
-//                url: "http://137.190.207.214:3000/temp/override" + targetTemp
-//            })
-//        }
-  });
+        this.getCurrentTemp = function (name) {
+            return $http({
+                method: "GET",
+                url: "http://137.190.207.214:3000/temp/current"
+            });
+        };
+
+        this.setCurrentTemp = function (temp) {
+
+            return $http({
+                method: "POST",
+                url: "http://137.190.207.214:3000/temp/override/" + temp
+            }).then(function (data) {
+                console.log(data);
+            })
+        }
+    });
