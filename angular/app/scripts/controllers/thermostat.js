c7e65a2b786d46d4a02ec99793eb01606e7d3b3d
maninacan <jon.r.aaron@gmail.com>
2015-01-16 19:05:37 -0700

    Working on indicator lights

--- angular/app/scripts/controllers/thermostat.js
+++ angular/app/scripts/controllers/thermostat.js
@@ -7,11 +7,128 @@
  * # ThermostatCtrl
  * Controller of the hotPieApp
  */
-angular.module('hotPieApp')
-  .controller('ThermostatCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+angular.module('hotPieApp').controller('ThermostatCtrl', function ($scope) {
+    $scope.indicatorDots = [
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;',
+        range: [99, 94]
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 220px;right: 10px;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      }
     ];
+    for (var i = 0; i < $scope.indicatorDots.length; i++) {
+
+    }
+  $scope = {
+    indicatorDots: [
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;',
+        range: [99, 94]
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 220px;right: 10px;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      },
+      {
+        on: false,
+        color: 'gray',
+        style: 'top: 0;right: 0;'
+      }
+    ],
+    name: '',
+    age: 457
+  };
   });
