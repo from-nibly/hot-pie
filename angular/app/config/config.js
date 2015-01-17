235236b6b5a8c383ad7793d2f911c40daf52299c
maninacan <jon.r.aaron@gmail.com>
2015-01-17 10:56:45 -0700

    Merges in changes from Jordan

--- angular/app/config/config.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by jonaaron on 1/17/15.
- */
---
+++ angular/app/scripts/services/config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name hotPieApp.config
+ * @description
+ * # config
+ * Service in the hotPieApp.
+ */
+angular.module('hotPieApp')
+  .service('config', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+    var host = '';
+    var port = '';
+    this.getHost = function() {
+      return host;
+    }
+  });
